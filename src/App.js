e7fc119a31816694b2b029d6655670846c16a358
patidarajay <patidar.ajay@gmail.com>
2019-01-17 21:52:28 -0500

    change comment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was changed on the feature branch !! 
+            Hello Sheik , how are you doing  
           </p>
           <a
             className="App-link"
